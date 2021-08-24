d4edcaf50f4ef84d8403c4918c1f36172d4c726f
so yeon <alicesysy@swu.ac.kr>
2021-08-24 20:39:41 +0900

    [feat] 웹 사이트 연결

--- SwiftUI_Webview_tutorial/SwiftUI_Webview_tutorial/ContentView.swift
+++ SwiftUI_Webview_tutorial/SwiftUI_Webview_tutorial/ContentView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            HStack {
+                NavigationLink(
+                    destination: MyWebview(urlToLoad: "https://www.naver.com")
+                        .edgesIgnoringSafeArea(.all)
+                ){
+                    Text("Naver")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(15)
+                }
+                
+                NavigationLink(
+                    destination: MyWebview(urlToLoad: "https://www.google.co.kr/")
+                        .edgesIgnoringSafeArea(.all)
+                ){
+                    Text("Google")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                        .padding()
+                        .background(Color.yellow)
+                        .cornerRadius(15)
+                }
+                
+                NavigationLink(
+                    destination: MyWebview(urlToLoad: "https://www.daum.net")
+                        .edgesIgnoringSafeArea(.all)
+                ){
+                    Text("Daum")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(15)
+                }
+            }
+        }
     }
 }
 
