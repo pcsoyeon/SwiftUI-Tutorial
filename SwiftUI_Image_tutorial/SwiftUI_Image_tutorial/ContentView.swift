cc471a62a02ad728abea614dcf244d91ec54dd77
so yeon <alicesysy@swu.ac.kr>
2021-08-25 17:11:35 +0900

    [feat] Webview 연결

--- SwiftUI_Image_tutorial/SwiftUI_Image_tutorial/ContentView.swift
+++ SwiftUI_Image_tutorial/SwiftUI_Image_tutorial/ContentView.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        CircleImageView()
+        NavigationView {
+            VStack {
+                Image("myImage")
+                    .frame(height: 10)
+                    .offset(y: -1100)
+                CircleImageView()
+                
+                HStack {
+                    NavigationLink(
+                        destination: MyWebview(urlToLoad: "https://www.naver.com")
+                            .edgesIgnoringSafeArea(.all)
+                    ){
+                        Text("Naver")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                            .padding()
+                            .background(Color.green)
+                            .cornerRadius(15)
+                    }
+                    
+                    NavigationLink(
+                        destination: MyWebview(urlToLoad: "https://www.google.co.kr/")
+                            .edgesIgnoringSafeArea(.all)
+                    ){
+                        Text("Google")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(15)
+                    }
+                    
+                    NavigationLink(
+                        destination: MyWebview(urlToLoad: "https://www.apple.com")
+                            .edgesIgnoringSafeArea(.all)
+                    ){
+                        Text("Apple")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                            .padding()
+                            .background(Color.black)
+                            .cornerRadius(15)
+                    }
+                }
+                .padding(50)
+            }
+        }
     }
 }
 
---
+++ SwiftUI_Image_tutorial/SwiftUI_Image_tutorial/MyWebview.swift
@@ -0,0 +1,41 @@
+//
+//  MyWebview.swift
+//  SwiftUI_Image_tutorial
+//
+//  Created by soyeon on 2021/08/25.
+//
+
+import SwiftUI
+import WebKit
+
+struct MyWebview: UIViewRepresentable {
+    var urlToLoad: String
+    
+    // UI View 생성
+    func makeUIView(context: Context) -> WKWebView {
+        // Unwrapping
+        guard let url = URL(string: urlToLoad) else {
+            return WKWebView()
+            
+        }
+        
+        // Webview 인스턴스 생성
+        let webView = WKWebView()
+        
+        // Webview 로드
+        webView.load(URLRequest(url: url))
+        
+        return webView
+    }
+    
+    // UI View Update
+    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<MyWebview>) {
+        
+    }
+}
+
+struct MyWebview_Previews: PreviewProvider {
+    static var previews: some View {
+        MyWebview(urlToLoad: "http://www.naver.com")
+    }
+}
